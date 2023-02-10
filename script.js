841b0f81a6a3bd29efc4da60ef24c66dfc0ff63f
Dorb22 <orbe.dan22@gmail.com>
2023-02-10 21:39:43 +1100

    Always return rounded off answer

--- script.js
+++ script.js
@@ -46,26 +46,36 @@ const operatorArray = Array.from(operator);
 
 operatorArray.forEach(button => {
     button.addEventListener('click', () =>{
+        if (oper) {
+            getAnswer();
+            oper = button.textContent;
+            input += button.textContent;
+            current.textContent = input;
+        }
+        else{
         oper = button.textContent;
         input += button.textContent;
         current.textContent = input;
-        console.log()
+        }
     })
 })
 
 const equals = document.querySelector('.equals');
-equals.addEventListener('click', () => {
+equals.addEventListener('click', getAnswer);
+
+function getAnswer(){
     let split = input.split(oper);
-    num1 = parseInt (split[0]);
-    num2 = parseInt (split[1]);
+    num1 = Number(split[0]);
+    num2 = Number(split[1]);
     if (oper === '+') answer = add(num1, num2);
     else if (oper ==='-') answer = subtract(num1, num2);
     else if (oper === 'x') answer = multiply(num1, num2);
     else if (oper === '÷') answer = divide(num1, num2);
     past.textContent = input;
-    current.textContent = answer;
+    current.textContent = (Math.round(answer * 10 ** 9))/10 ** 9;
     input = current.textContent;
-})
+    oper = '';
+}
 
 const del = document.querySelector('.delete');
 del.addEventListener('click', () =>{
@@ -78,3 +88,4 @@ const clear = document.querySelector('.clear');
 clear.addEventListener('click', () => {
     input = num1 = num2 = oper = answer = current.textContent = past.textContent = '';
 })
+
