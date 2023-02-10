bf5cd71bf11124903e5b3f5303d944ba5ed2b7fd
Dorb22 <orbe.dan22@gmail.com>
2023-02-10 15:41:38 +1100

    Make basic math functions

---
+++ script.js
@@ -0,0 +1,23 @@
+function add (num1, num2){
+    let total = num1 + num2; 
+    return total;   
+}
+
+function subtract (num1, num2){
+    let total = num1 - num2; 
+    return total;   
+}
+
+function multiply (num1, num2){
+    let total = num1 * num2; 
+    return total;   
+}
+
+function divide (num1, num2){
+    let total = num1 / num2; 
+    return total;   
+}
+
+console.log(multiply(10,2));
+
+
