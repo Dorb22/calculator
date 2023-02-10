080498d7a19d45102ba7a8dbbf5e9d121acf0900
Dorb22 <orbe.dan22@gmail.com>
2023-02-10 18:49:59 +1100

    Show input on display when clicked

--- script.js
+++ script.js
@@ -25,6 +25,40 @@ function operate (operator, num1, num2){
     else if (operator === '/') return divide(num1, num2);
 }
 
+let input = '';
+let num1, num2, nbr, oper;
+const num = document.querySelectorAll('.number');
+const numArray = Array.from(num);
+const current = document.querySelector('.current')
+current.textContent = input;
+
+numArray.forEach(button =>{
+    button.addEventListener('click', () =>{
+        if(!num1){
+        input = '';
+        input += button.textContent;
+        nbr = input;
+        current.textContent = input;
+        }
+        else  
+    })
+})
+
+
+
+const operator = document.querySelectorAll('.operator');
+const operatorArray = Array.from(operator);
+
+operatorArray.forEach(button => {
+    button.addEventListener('click', () =>{
+        oper = button.textContent;
+        num1 = parseInt(nbr);
+        input += button.textContent;
+        current.textContent = input;
+        console.log(input);
+    })
+})
+
 
 
 
