914b6991e250fa2704e67613d78426ee36f406e9
Dorb22 <orbe.dan22@gmail.com>
2023-02-10 15:49:38 +1100

    Write operate function

--- script.js
+++ script.js
@@ -18,6 +18,14 @@ function divide (num1, num2){
     return total;   
 }
 
-console.log(multiply(10,2));
+function operate (operator, num1, num2){
+    if (operator === '+') return add(num1, num2);
+    else if (operator === '-') return subtract(num1, num2);
+    else if (operator === '*') return multiply(num1, num2);
+    else if (operator === '/') return divide(num1, num2);
+}
+
+
+
 
 
